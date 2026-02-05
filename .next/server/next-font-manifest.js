0e6d9d18818ba18f12ee09e3385fc3257fe11034
MariamAkalay <mariam.akalay@gmail.com>
2026-02-05 17:04:51 +0100

    added framer-motion

---
+++ .next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/home/MariamAkalay/testrestau_site/app/layout\":[\"static/media/6c596dfcddeca1e9-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"959jvg3qV0zEViYo2RWLr0HohFF4X+vg/bm2kHOavwc=\"\n}"
